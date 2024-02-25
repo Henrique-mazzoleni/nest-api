7e974a4f9b8721f1e013a978937d4699d2511ac7
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2024-02-25 21:18:06 +0100

    e2e test for auth implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -12,6 +12,7 @@ export class AuthController {
   }
 
   @Post('signin')
+  @HttpCode(200)
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
   }
